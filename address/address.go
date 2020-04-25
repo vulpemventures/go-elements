5382f3ea0db3ee3bcf76fc11400891b811ce3553
Marco Argentieri <marco@vulpem.com>
2020-04-25 02:12:35 +0200

    Add Bech32 function

--- address/address.go
+++ address/address.go
@@ -3,6 +3,8 @@ package address
 import (
 	"errors"
 	"github.com/btcsuite/btcutil/base58"
+	"github.com/btcsuite/btcutil/bech32"
+	
 )
 
 // Address defines the address as string 
@@ -17,7 +19,6 @@ type Base58 struct {
 
 // Bech32 defines the structure of an address native segwit
 type Bech32 struct {
-	Version byte
 	Prefix string
 	Data []byte 
 }
@@ -50,4 +51,30 @@ func FromBase58(address string) (*Base58, error) {
 func ToBase58(b *Base58) string {
 	encoded := base58.CheckEncode(b.Data, b.Version)
 	return encoded
+}
+
+
+// FromBech32 decodes a bech32 encoded string, returning the human-readable
+// part and the data part excluding the checksum.
+func FromBech32(address string) (*Bech32, error) {
+	hrp, decoded, err := bech32.Decode(address)
+	if err != nil {
+		return nil, errors.New("Invalid address")
+	}
+
+	return &Bech32{hrp, decoded}, nil
+}
+
+// ToBech32 encodes a byte slice into a bech32 string with the
+// human-readable part hrb. Note that the bytes must each encode 5 bits
+func ToBech32(bc *Bech32) (string, error) {
+	conv, err := bech32.ConvertBits(bc.Data, 8, 5, true)
+	if err != nil {
+		return "", errors.New("Invalid bech32 conversion")
+	}
+	encoded, err := bech32.Encode(bc.Prefix, conv)
+	if err != nil {
+		return "", errors.New("Invalid bech32 encoding")
+	}
+	return encoded, nil
 }
--- address/address_test.go
+++ address/address_test.go
@@ -9,6 +9,8 @@ import (
 const (
 	base58address = "XFKcLWJmPuToz62uc2sgCBUddmH6yopoxE"
 	base58hexdata = "2b919bfc040faed8de5469dfa0241a3c1e5681be"
+	bech32hexdata = "e6a10b7bd8aeb56444c5734ea682cd2f1ad692c4"
+	bech32address = "ert1qu6ssk77c466kg3x9wd82dqkd9udddykyfykm9k"
 )
 
 func TestFromBase58(t *testing.T) {
@@ -33,4 +35,21 @@ func TestToBase58(t *testing.T) {
 	if addr != base58address {
 		t.Errorf("TestToBase58: base58 encoding error")
 	}
+}
+
+func TestBech32(t *testing.T) {
+	bech32, err := address.FromBech32(bech32address)
+	if err != nil {
+		t.Errorf("TestFromBech32: bech32 decoding error")
+	}
+	if bech32.Prefix != "ert" {
+		t.Errorf("TestBech32: wrong prefix")
+	}
+	if len(bech32.Data) != 33 {
+		t.Errorf("TestBech32: data size mismatch")
+	}
+	data, _ := hex.DecodeString(bech32hexdata)
+	payload := &address.Bech32{"ert", data}
+	addr, _ := address.ToBech32(payload)
+	println(addr)
 }

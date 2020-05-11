1e96626ea7a265c0298726a546443e97f7ae3a62
altafan <pietro@vulpem.com>
2020-05-11 12:50:11 +0200

    Add helpers

--- transaction/common.go
+++ transaction/common.go
@@ -260,3 +260,32 @@ func readVarInt(r io.Reader) (uint64, error) {
 
 	return rv, nil
 }
+
+// varIntSerializeSize returns the number of bytes it would take to serialize
+// val as a variable length integer.
+func varIntSerializeSize(val uint64) int {
+	// The value is small enough to be represented by itself, so it's
+	// just 1 byte.
+	if val < 0xfd {
+		return 1
+	}
+
+	// Discriminant 1 byte plus 2 bytes for the uint16.
+	if val <= math.MaxUint16 {
+		return 3
+	}
+
+	// Discriminant 1 byte plus 4 bytes for the uint32.
+	if val <= math.MaxUint32 {
+		return 5
+	}
+
+	// Discriminant 1 byte plus 8 bytes for the uint64.
+	return 9
+}
+
+// varSlizeSerializeSize returns the number of bytes it would take to serialize
+// val as a variable length byte slice.
+func varSliceSerializeSize(val []byte) int {
+	return varIntSerializeSize(uint64(len(val))) + len(val)
+}

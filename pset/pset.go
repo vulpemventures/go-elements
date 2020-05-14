7d668a8e4eddbbef7bb71d1a1a180e0d2a7f3711
altafan <pietro@vulpem.com>
2020-05-14 11:39:38 +0200

    Add constants for serialization and error messages

--- pset/pset.go
+++ pset/pset.go
@@ -1,7 +1,98 @@
+// Copyright (c) 2013-2017 The btcsuite developers
+// Copyright (c) 2015-2016 The Decred developers
+// Copyright (c) 2019-2020 The VulpemVentures developers
+
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 package pset
 
 import (
 	"encoding/base64"
+	"errors"
+)
+
+// MaxPsbtValueLength is the size of the largest transaction serialization
+// that could be passed in a NonWitnessUtxo field. This is definitely
+//less than 4M.
+const MaxPsbtValueLength = 4000000
+
+// MaxPsbtKeyLength is the length of the largest key that we'll successfully
+// deserialize from the wire. Anything more will return ErrInvalidKeydata.
+const MaxPsbtKeyLength = 10000
+
+var (
+	// ErrInvalidPsbtFormat is a generic error for any situation in which a
+	// provided Psbt serialization does not conform to the rules of BIP174.
+	ErrInvalidPsbtFormat = errors.New("Invalid PSBT serialization format")
+
+	// ErrDuplicateKey indicates that a passed Psbt serialization is invalid
+	// due to having the same key repeated in the same key-value pair.
+	ErrDuplicateKey = errors.New("Invalid Psbt due to duplicate key")
+
+	// ErrInvalidKeydata indicates that a key-value pair in the PSBT
+	// serialization contains data in the key which is not valid.
+	ErrInvalidKeydata = errors.New("Invalid key data")
+
+	// ErrInvalidMagicBytes indicates that a passed Psbt serialization is invalid
+	// due to having incorrect magic bytes.
+	ErrInvalidMagicBytes = errors.New("Invalid Psbt due to incorrect magic bytes")
+
+	// ErrInvalidRawTxSigned indicates that the raw serialized transaction in the
+	// global section of the passed Psbt serialization is invalid because it
+	// contains scriptSigs/witnesses (i.e. is fully or partially signed), which
+	// is not allowed by BIP174.
+	ErrInvalidRawTxSigned = errors.New("Invalid Psbt, raw transaction must " +
+		"be unsigned.")
+
+	// ErrInvalidPrevOutNonWitnessTransaction indicates that the transaction
+	// hash (i.e. SHA256^2) of the fully serialized previous transaction
+	// provided in the NonWitnessUtxo key-value field doesn't match the prevout
+	// hash in the UnsignedTx field in the PSBT itself.
+	ErrInvalidPrevOutNonWitnessTransaction = errors.New("Prevout hash does " +
+		"not match the provided non-witness utxo serialization")
+
+	// ErrInvalidSignatureForInput indicates that the signature the user is
+	// trying to append to the PSBT is invalid, either because it does
+	// not correspond to the previous transaction hash, or redeem script,
+	// or witness script.
+	// NOTE this does not include ECDSA signature checking.
+	ErrInvalidSignatureForInput = errors.New("Signature does not correspond " +
+		"to this input")
+
+	// ErrInputAlreadyFinalized indicates that the PSBT passed to a Finalizer
+	// already contains the finalized scriptSig or witness.
+	ErrInputAlreadyFinalized = errors.New("Cannot finalize PSBT, finalized " +
+		"scriptSig or scriptWitnes already exists")
+
+	// ErrIncompletePSBT indicates that the Extractor object
+	// was unable to successfully extract the passed Psbt struct because
+	// it is not complete
+	ErrIncompletePSBT = errors.New("PSBT cannot be extracted as it is " +
+		"incomplete")
+
+	// ErrNotFinalizable indicates that the PSBT struct does not have
+	// sufficient data (e.g. signatures) for finalization
+	ErrNotFinalizable = errors.New("PSBT is not finalizable")
+
+	// ErrInvalidSigHashFlags indicates that a signature added to the PSBT
+	// uses Sighash flags that are not in accordance with the requirement
+	// according to the entry in PsbtInSighashType, or otherwise not the
+	// default value (SIGHASH_ALL)
+	ErrInvalidSigHashFlags = errors.New("Invalid Sighash Flags")
+
+	// ErrUnsupportedScriptType indicates that the redeem script or
+	// scriptwitness given is not supported by this codebase, or is otherwise
+	// not valid.
+	ErrUnsupportedScriptType = errors.New("Unsupported script type")
 )
 
 // Unknown is a struct encapsulating a key-value pair for which the key type is unknown

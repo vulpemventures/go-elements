a9f4833b091037bdc37efdf1b6baeebb7e6131a9
Dusan Sekulic <sekula87@gmail.com>
2020-05-19 12:29:20 +0200

    Payment: FromScript factory method (#38)
    * FromScript factory method added
    
    * network validation added
    
    * refactor after review

--- payment/payment.go
+++ payment/payment.go
@@ -29,12 +29,18 @@ type Payment struct {
 // pub 036f5646ed688b9279369da0a4ad78953ae7e6d300436ca8a3264360efe38236e3
 
 // FromPublicKey creates a Payment struct from a btcec.publicKey
-func FromPublicKey(pubkey *btcec.PublicKey, network *network.Network) *Payment {
+func FromPublicKey(pubkey *btcec.PublicKey, net *network.Network) *Payment {
+	var tmpNet *network.Network
+	if net == nil {
+		tmpNet = &network.Liquid
+	} else {
+		tmpNet = net
+	}
 	publicKeyBytes := pubkey.SerializeCompressed()
 	hash := hash160(publicKeyBytes)[:ripemd160.Size]
 	script := make([]byte, 0)
 	script = append([]byte{Op0, byte(len(hash))}, hash...)
-	return &Payment{network, pubkey, hash, nil, nil, script}
+	return &Payment{tmpNet, pubkey, hash, nil, nil, script}
 }
 
 // FromPayment creates a Payment struct from a another Payment
@@ -44,6 +50,18 @@ func FromPayment(payment *Payment) (*Payment, error) {
 	}
 	redeem := &Payment{payment.Network, payment.PublicKey, payment.Hash, payment.BlindingKey, payment.Redeem, payment.Script}
 	return &Payment{payment.Network, nil, hash160(redeem.Script), nil, redeem, nil}, nil
+}
+
+// FromPayment creates a nested Payment struct from script
+func FromScript(script []byte, net *network.Network) (*Payment, error) {
+	var tmpNet *network.Network
+	if net == nil {
+		tmpNet = &network.Liquid
+	} else {
+		tmpNet = net
+	}
+	redeem := &Payment{Network: tmpNet, Script: script}
+	return FromPayment(redeem)
 }
 
 // PubKeyHash is a method of the Payment struct to derive a base58 p2pkh address

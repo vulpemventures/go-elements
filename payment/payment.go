13b6d0f0f0182ff355ec7f5e51c704904b2b6747
Dusan Sekulic <sekula87@gmail.com>
2020-05-20 11:48:30 +0200

    Payment: p2wsh address (#40)
    * p2wsh address added
    
    * add witness hash field to Payment type

--- payment/payment.go
+++ payment/payment.go
@@ -22,6 +22,7 @@ type Payment struct {
 	BlindingKey *btcec.PublicKey
 	Redeem      *Payment
 	Script      []byte
+	WitnessHash []byte
 }
 
 //target mail duty exit light void budget zone senior tag rude wisdom
@@ -37,23 +38,28 @@ func FromPublicKey(pubkey *btcec.PublicKey, net *network.Network) *Payment {
 		tmpNet = net
 	}
 	publicKeyBytes := pubkey.SerializeCompressed()
-	hash := hash160(publicKeyBytes)[:ripemd160.Size]
+	pkHash := hash160(publicKeyBytes)[:ripemd160.Size]
 	script := make([]byte, 0)
-	script = append([]byte{Op0, byte(len(hash))}, hash...)
-	return &Payment{tmpNet, pubkey, hash, nil, nil, script}
+	script = append([]byte{Op0, byte(len(pkHash))}, pkHash...)
+	witnessHash := sha256.Sum256(script)
+	return &Payment{tmpNet, pubkey, pkHash, nil, nil, script, witnessHash[:]}
 }
 
 // FromPayment creates a Payment struct from a another Payment
 func FromPayment(payment *Payment) (*Payment, error) {
-	if payment.Script == nil {
-		return nil, errors.New("script can't be nil")
+	if payment.Script == nil || len(payment.Script) == 0 {
+		return nil, errors.New("payment's script can't be empty or nil")
 	}
-	redeem := &Payment{payment.Network, payment.PublicKey, payment.Hash, payment.BlindingKey, payment.Redeem, payment.Script}
-	return &Payment{payment.Network, nil, hash160(redeem.Script), nil, redeem, nil}, nil
+	redeem := &Payment{payment.Network, payment.PublicKey, payment.Hash, payment.BlindingKey, payment.Redeem, payment.Script, payment.WitnessHash}
+	witnessHash := sha256.Sum256(redeem.Script)
+	return &Payment{payment.Network, payment.PublicKey, hash160(redeem.Script), payment.BlindingKey, redeem, payment.Script, witnessHash[:]}, nil
 }
 
 // FromPayment creates a nested Payment struct from script
 func FromScript(script []byte, net *network.Network) (*Payment, error) {
+	if script == nil || len(script) == 0 {
+		return nil, errors.New("payment's script can't be empty or nil")
+	}
 	var tmpNet *network.Network
 	if net == nil {
 		tmpNet = &network.Liquid
@@ -66,28 +72,55 @@ func FromScript(script []byte, net *network.Network) (*Payment, error) {
 
 // PubKeyHash is a method of the Payment struct to derive a base58 p2pkh address
 func (p *Payment) PubKeyHash() string {
+	if p.Hash == nil || len(p.Hash) == 0 {
+		errors.New("payment's hash can't be empty or nil")
+	}
 	payload := &address.Base58{p.Network.PubKeyHash, p.Hash}
 	addr := address.ToBase58(payload)
 	return addr
 }
 
+// ScriptHash is a method of the Payment struct to derive a base58 p2sh address
+func (p *Payment) ScriptHash() (string, error) {
+	if p.Hash == nil || len(p.Hash) == 0 {
+		return "", errors.New("payment's hash can't be empty or nil")
+	}
+	payload := &address.Base58{p.Network.ScriptHash, p.Hash}
+	addr := address.ToBase58(payload)
+	return addr, nil
+}
+
 // WitnessPubKeyHash is a method of the Payment struct to derive a base58 p2wpkh address
-func (p *Payment) WitnessPubKeyHash() string {
+func (p *Payment) WitnessPubKeyHash() (string, error) {
+	if p.Hash == nil || len(p.Hash) == 0 {
+		return "", errors.New("payment's hash can't be empty or nil")
+	}
 	//Here the Version for wpkh is always 0
 	version := byte(0x00)
 	payload := &address.Bech32{p.Network.Bech32, version, p.Hash}
 	addr, err := address.ToBech32(payload)
 	if err != nil {
-		return ""
+		return "", nil
 	}
-	return addr
+	return addr, nil
 }
 
-// ScriptHash is a method of the Payment struct to derive a base58 p2sh address
-func (p *Payment) ScriptHash() string {
-	payload := &address.Base58{p.Network.ScriptHash, p.Hash}
-	addr := address.ToBase58(payload)
-	return addr
+// WitnessScriptHash is a method of the Payment struct to derive a base58 p2wsh address
+func (p *Payment) WitnessScriptHash() (string, error) {
+	if p.Script == nil || len(p.Script) == 0 {
+		return "", errors.New("payment's script can't be empty or nil")
+	}
+	if p.WitnessHash == nil || len(p.WitnessHash) == 0 {
+		return "", errors.New("payment's witnessHash can't be empty or nil")
+	}
+
+	version := byte(0x00)
+	payload := &address.Bech32{p.Network.Bech32, version, p.WitnessHash}
+	addr, err := address.ToBech32(payload)
+	if err != nil {
+		return "", err
+	}
+	return addr, nil
 }
 
 // Calculate the hash of hasher over buf.

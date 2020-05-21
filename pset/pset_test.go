75fe723b6c1eeeee699047d5c46af0105de5858e
altafan <pietro@vulpem.com>
2020-05-21 11:35:13 +0200

    Add comments

--- pset/pset_test.go
+++ pset/pset_test.go
@@ -300,6 +300,20 @@ func TestExtractor(t *testing.T) {
 }
 
 func TestFromCreateToBroadcast(t *testing.T) {
+	/**
+	* This test attempts to broadcast a transaction composed by 1 input and 3
+	* outputs. The input of the transaction will be a native segwit input, thus
+	* locked by a p2wpkh script, while the outputs will be a legacy p2sh for the
+	* receiver and a segwit p2wpkh for the change.
+	* The 3rd output is for the fees, that in Elements side chains are explicits.
+	*
+	* This is intended to test that all methods provided let one to manage a
+	* partial transaction from its creatation to the extraction of the final
+	* tranasction so that it can be correctly broadcasted to the network and
+	* included in the blockchain.
+	 */
+
+	// Generate sender random key pair.
 	privkey, err := btcec.NewPrivateKey(btcec.S256())
 	if err != nil {
 		t.Fatal(err)
@@ -308,16 +322,19 @@ func TestFromCreateToBroadcast(t *testing.T) {
 	p2wpkh := payment.FromPublicKey(pubkey, &network.Regtest)
 	address, _ := p2wpkh.WitnessPubKeyHash()
 
+	// Fund sender address.
 	_, err = faucet(address)
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	// Retrieve sender utxos.
 	utxos, err := unspents(address)
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	// The transaction will have 1 input and 3 outputs.
 	txInputHash, _ := hex.DecodeString(utxos[0]["txid"].(string))
 	txInputHash = reverseBytes(txInputHash)
 	txInputIndex := uint32(utxos[0]["vout"].(float64))
@@ -337,6 +354,7 @@ func TestFromCreateToBroadcast(t *testing.T) {
 	feeValue, _ := toConfidentialValue(500)
 	feeOutput := transaction.NewTxOutput(lbtc, feeValue, feeScript)
 
+	// Create a new pset.
 	inputs := []*transaction.TxInput{txInput}
 	outputs := []*transaction.TxOutput{receiverOutput, changeOutput, feeOutput}
 	p, err := New(inputs, outputs, 2, 0)
@@ -344,6 +362,7 @@ func TestFromCreateToBroadcast(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	// Add sighash type and witness utxo to the partial input.
 	updater, err := NewUpdater(p)
 	if err != nil {
 		t.Fatal(err)
@@ -357,6 +376,11 @@ func TestFromCreateToBroadcast(t *testing.T) {
 	witnessUtxo := transaction.NewTxOutput(lbtc, witValue, p2wpkh.Script)
 	updater.AddInWitnessUtxo(witnessUtxo, 0)
 
+	// The signing of the input is done by retrieving the proper hash of the serialization
+	// of the transaction (the BIP-0143 segwit version in this case) directly from the pset's
+	// UnsignedTx.
+	// NOTE: to correctly sign an utxo locked by a p2wpkh script, we must use the legacy pubkey script
+	// when serializing the transaction.
 	legacyScript := append(append([]byte{0x76, 0xa9, 0x14}, p2wpkh.Hash...), []byte{0x88, 0xac}...)
 	witHash := updater.Upsbt.UnsignedTx.HashForWitnessV0(0, legacyScript, witValue, txscript.SigHashAll)
 	sig, err := privkey.Sign(witHash[:])
@@ -367,24 +391,31 @@ func TestFromCreateToBroadcast(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	// Update the pset adding the input signature script and the pubkey.
 	_, err = updater.Sign(0, sigWithHashType, pubkey.SerializeCompressed(), nil, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	// Finalize the partial transaction.
 	p = updater.Upsbt
 	err = FinalizeAll(p)
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	// Extract the final signed transaction from the Pset wrapper.
 	finalTx, err := Extract(p)
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	// Serialize the transaction and try to broadcast.
 	txHex, err := finalTx.ToHex()
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	txid, err := broadcast(txHex)
 	if err != nil {
 		t.Fatal(err)

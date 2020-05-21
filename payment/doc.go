5d18d9da1d54bc52a39c591afda6ed5a8a8190e0
sekulicd <sekula87@gmail.com>
2020-05-21 10:55:10 +0200

    payment documentation

---
+++ payment/doc.go
@@ -0,0 +1,9 @@
+/*
+Package payment is an abstract for working with all kind of addresses in Liquid network.
+
+It can be used for the creation of p2pkh, p2ms, p2sh, non-native SegWit and native SegWit addresses.
+
+It also provides support for confidential payments.
+
+*/
+package payment

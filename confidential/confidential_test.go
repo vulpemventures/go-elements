e057e3c9412e53c95ee1e4769d04a5c11f86777a
Dusan Sekulic <sekula87@gmail.com>
2020-06-01 14:49:57 +0200

    Confidential: method for final value blinding factor (#70)
    * added factory method FromPublicKeys for creating multiscript payment
    
    * FinalValueBlindingFactor method added
    
    * FinalValueBlindingFactor tcs added
    
    * refactor after review

--- confidential/confidential_test.go
+++ confidential/confidential_test.go
@@ -10,20 +10,29 @@ import (
 	"testing"
 )
 
-func TestUnblindOutput(t *testing.T) {
+var tests map[string]interface{}
+
+func setUp() error {
 	file, err := ioutil.ReadFile("data/confidential.json")
 	if err != nil {
-		t.Fatal(err)
+		return err
 	}
 
-	var tests map[string]interface{}
 	err = json.Unmarshal(file, &tests)
 	if err != nil {
-		t.Error(err)
+		return err
 	}
 
-	vectors := tests["unblind"].([]interface{})
+	return nil
+}
 
+func TestUnblindOutput(t *testing.T) {
+	err := setUp()
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+
+	vectors := tests["unblindOutput"].([]interface{})
 	for _, testVector := range vectors {
 		v := testVector.(map[string]interface{})
 		scriptPubkeyStr := v["scriptPubkey"].(string)
@@ -34,22 +43,34 @@ func TestUnblindOutput(t *testing.T) {
 		rangeproofStr := v["rangeproof"].(string)
 
 		ephemeralPubkey, err := hex.DecodeString(ephemeralPubkeyStr)
-		assert.NoError(t, err)
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
 
 		blindingPrivkey, err := hex.DecodeString(blindingPrivkeyStr)
-		assert.NoError(t, err)
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
 
 		rangeproof, err := hex.DecodeString(rangeproofStr)
-		assert.NoError(t, err)
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
 
 		commitment, err := secp256k1.CommitmentFromString(valueCommitmentStr)
-		assert.NoError(t, err)
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
 
 		assetGenerator, err := hex.DecodeString(assetGeneratorStr)
-		assert.NoError(t, err)
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
 
 		scriptPubkey, err := hex.DecodeString(scriptPubkeyStr)
-		assert.NoError(t, err)
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
 
 		input := UnblindInput{
 			EphemeralPubkey: ephemeralPubkey,
@@ -61,13 +82,17 @@ func TestUnblindOutput(t *testing.T) {
 		}
 
 		output, err := UnblindOutput(input)
-		assert.NoError(t, err)
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
 
 		expected := v["expected"].(map[string]interface{})
 
 		valueStr := expected["value"].(string)
 		value, err := strconv.Atoi(valueStr)
-		assert.NoError(t, err)
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
 
 		assetStr := expected["asset"].(string)
 		valueBlindingFactor := expected["valueBlindingFactor"].(string)
@@ -77,5 +102,94 @@ func TestUnblindOutput(t *testing.T) {
 		assert.Equal(t, hex.EncodeToString(output.AssetBlindingFactor), assetBlindingFactor)
 		assert.Equal(t, hex.EncodeToString(output.Asset), assetStr)
 		assert.Equal(t, hex.EncodeToString(output.ValueBlindingFactor[:]), valueBlindingFactor)
+	}
+}
+
+func TestFinalValueBlindingFactor(t *testing.T) {
+	err := setUp()
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+
+	vectors := tests["finalValueBlindingFactor"].([]interface{})
+	for _, testVector := range vectors {
+		v := testVector.(map[string]interface{})
+
+		inValuesSlice := v["inValues"].([]interface{})
+		inValues := make([]uint64, 0)
+		for _, val := range inValuesSlice {
+			n, err := strconv.ParseUint(val.(string), 10, 64)
+			if !assert.NoError(t, err) {
+				t.FailNow()
+			}
+			inValues = append(inValues, n)
+		}
+
+		outValuesSlice := v["outValues"].([]interface{})
+		outValues := make([]uint64, 0)
+		for _, val := range outValuesSlice {
+			n, err := strconv.ParseUint(val.(string), 10, 64)
+			if !assert.NoError(t, err) {
+				t.FailNow()
+			}
+			outValues = append(outValues, n)
+		}
+
+		inGeneratorsSlice := v["inGenerators"].([]interface{})
+		inGenerators := make([][]byte, 0)
+		for _, val := range inGeneratorsSlice {
+			gen, err := hex.DecodeString(val.(string))
+			if !assert.NoError(t, err) {
+				t.FailNow()
+			}
+			inGenerators = append(inGenerators, gen)
+		}
+
+		outGeneratorsSlice := v["outGenerators"].([]interface{})
+		outGenerators := make([][]byte, 0)
+		for _, val := range outGeneratorsSlice {
+			gen, err := hex.DecodeString(val.(string))
+			if !assert.NoError(t, err) {
+				t.FailNow()
+			}
+			outGenerators = append(outGenerators, gen)
+		}
+
+		inFactorsSlice := v["inFactors"].([]interface{})
+		inFactors := make([][]byte, 0)
+		for _, val := range inFactorsSlice {
+			gen, err := hex.DecodeString(val.(string))
+			if !assert.NoError(t, err) {
+				t.FailNow()
+			}
+			inFactors = append(inFactors, gen)
+		}
+
+		outFactorsSlice := v["outFactors"].([]interface{})
+		outFactors := make([][]byte, 0)
+		for _, val := range outFactorsSlice {
+			gen, err := hex.DecodeString(val.(string))
+			if !assert.NoError(t, err) {
+				t.FailNow()
+			}
+			outFactors = append(outFactors, gen)
+		}
+
+		input := FinalValueBlindingFactorInput{
+			InValues:      inValues,
+			OutValues:     outValues,
+			InGenerators:  inGenerators,
+			OutGenerators: outGenerators,
+			InFactors:     inFactors,
+			OutFactors:    outFactors,
+		}
+
+		factor, err := FinalValueBlindingFactor(input)
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
+
+		expectedFactor := v["expected"].(string)
+		assert.Equal(t, expectedFactor, hex.EncodeToString(factor[:]))
 	}
 }

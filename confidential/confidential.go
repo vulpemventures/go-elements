f389cf2abb32df764c4cd20c3fc1577ecfd3082d
sekulicd <sekula87@gmail.com>
2020-06-02 17:10:14 +0200

    SurjectionProof method added

--- confidential/confidential.go
+++ confidential/confidential.go
@@ -91,3 +91,65 @@ func FinalValueBlindingFactor(input FinalValueBlindingFactorInput) ([32]byte, er
 
 	return secp256k1.BlindGeneratorBlindSum(ctx, values, generatorBlind, blindingFactor, len(input.InValues))
 }
+
+type SurjectionProofInput struct {
+	OutputAsset               []byte
+	OutputAssetBlindingFactor []byte
+	InputAssets               [][]byte
+	InputAssetBlindingFactors [][]byte
+	Seed                      []byte
+}
+
+//SurjectionProof method generates surjection proof
+func SurjectionProof(input SurjectionProofInput) ([]byte, error) {
+	ctx, _ := secp256k1.ContextCreate(secp256k1.ContextBoth)
+	defer secp256k1.ContextDestroy(ctx)
+
+	outputGenerator, err := secp256k1.GeneratorGenerateBlinded(ctx, input.OutputAsset, input.OutputAssetBlindingFactor)
+	if err != nil {
+		return nil, err
+	}
+
+	inputGenerators := make([]secp256k1.Generator, 0)
+	for i, v := range input.InputAssets {
+		gen, err := secp256k1.GeneratorGenerateBlinded(ctx, v, input.InputAssetBlindingFactors[i])
+		if err != nil {
+			return nil, err
+		}
+		inputGenerators = append(inputGenerators, *gen)
+	}
+
+	fixedInputTags := make([]secp256k1.FixedAssetTag, 0)
+	for _, inTag := range input.InputAssets {
+		fixedAssetTag, err := secp256k1.FixedAssetTagParse(inTag)
+		if err != nil {
+			return nil, err
+		}
+		fixedInputTags = append(fixedInputTags, *fixedAssetTag)
+	}
+
+	var nInputsToUse int
+	if len(input.InputAssets) > 3 {
+		nInputsToUse = 3
+	} else {
+		nInputsToUse = len(input.InputAssets)
+	}
+
+	fixedOutputTag, err := secp256k1.FixedAssetTagParse(input.OutputAsset)
+	if err != nil {
+		return nil, err
+	}
+
+	maxIterations := 100
+	proof, inputIndex, err := secp256k1.SurjectionProofInitialize(ctx, fixedInputTags, nInputsToUse, *fixedOutputTag, maxIterations, input.Seed)
+	if err != nil {
+		return nil, err
+	}
+
+	err = secp256k1.SurjectionProofGenerate(ctx, proof, inputGenerators, *outputGenerator, inputIndex, input.InputAssetBlindingFactors[inputIndex], input.OutputAssetBlindingFactor)
+	if err != nil {
+		return nil, err
+	}
+
+	return secp256k1.SurjectionProofSerialize(ctx, proof)
+}

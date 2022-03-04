93923a0c1c0fc0133e749dedc4ab8c43469b00e8
altafan <pietro@vulpem.com>
2022-03-04 15:16:41 +0100

    Add tests for creator role

--- psetv2/utils_test.go
+++ psetv2/utils_test.go
@@ -2,10 +2,12 @@ package psetv2_test
 
 import (
 	"bytes"
+	"crypto/rand"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/big"
 	"net/http"
 	"os"
 	"strings"
@@ -286,4 +288,27 @@ func broadcast(txHex string) (string, error) {
 		return "", fmt.Errorf("failed to broadcast tx: %s", res)
 	}
 	return res, nil
+}
+
+func randomHex(len int) string {
+	return hex.EncodeToString(randomBytes(len))
+}
+
+func randomVout() uint32 {
+	return uint32(randomIntInRange(0, 15))
+}
+
+func randomValue() uint64 {
+	return uint64(randomIntInRange(1000000, 10000000000))
+}
+
+func randomBytes(len int) []byte {
+	b := make([]byte, len)
+	rand.Read(b)
+	return b
+}
+
+func randomIntInRange(min, max int) int {
+	n, _ := rand.Int(rand.Reader, big.NewInt(int64(max)))
+	return int(int(n.Int64())) + min
 }
